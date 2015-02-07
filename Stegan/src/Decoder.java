dc637279b622dc7ca05f05332f4a35f148ef6048
William Liddy <wjl36@case.edu>
2015-02-06 22:04:54 -0500

    Commit number 1

---
+++ Stegan/src/Decoder.java
@@ -0,0 +1,11 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Decoder {
+//lol get rekkt m8
+BufferedImage img;
+
+	public Decoder(File f){
+	img = new ImageIO.read(new File("strawberry.jpg"));
+
+}
